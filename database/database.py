ba34af337f5ee67d26624dc5df2719db3205c2ad
Aditya <gamedevlopers@gmail.com>
2023-07-19 14:24:09 +1000

    database models written

--- database/database.py
+++
@@ -1,3 +0,0 @@
-#intiialise database
-def init_db():
-    return sqlite3.connect(DATABASE)
---
+++ database/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    public_key = Column(String, nullable=False)
+    private_key = Column(String, nullable=False)
+
+    messages_sent = relationship("Message", backref='sender', foreign_keys='Message.sender_id')
+    messages_received = relationship("Message", backref='receiver', foreign_keys='Message.receiver_id')
+
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True)
+    sender_id = Column(Integer, ForeignKey('users.id'))
+    receiver_id = Column(Integer, ForeignKey('users.id'))
+    content = Column(String, nullable=False)
+    timestamp = Column(DateTime, nullable=False)
