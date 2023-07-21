57b1f202a44be84cab110eb795e4f0e50195d9cb
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-22 02:47:19 +1000

    base changes to app

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 from flask import Flask
+from flask_session import Session
+
 
 app = Flask(__name__)
+
+#configuring the session
+
+app.config['SESSION_PERMANENT']=False
+app.config['SESSION_TYPE']='filesystem'
+Session(app)
+
+#Import the routes
+
+from routes import index, chat, user
+
+if __name__ == '__main__':
+    app.run(debug=True)
