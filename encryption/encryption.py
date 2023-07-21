6b899b53b8d7fdd5a43a3941f5a2a9c4f3b913ae
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-22 01:23:20 +1000

    reformated file

--- encryption/encryption.py
+++ encryption/encryption.py
@@ -1,18 +1,33 @@
-from cryptography.hazmat.primitives import serialization 
-from cryptography.hazmat.primitives import rsa
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import padding
 
+
 def generate_key_pair():
-    private_key =rsa.generate_private_key(public_exponent = 65537, key_size = 2048)
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     public_key = private_key.public_key()
     return private_key, public_key
-    
+
+
 def encrypt_message(public_key, message):
-    encrypted_message = public_key.encrypt(message, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256(), label=NONE))
+    encrypted_message = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
     return encrypted_message
-    
+
+
 def decrypt_message(private_key, encrypted_message):
-    decrypted_message = private_key.decrypt(message, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256(), label=NONE))
+    decrypted_message = private_key.decrypt(
+        encrypted_message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
     return decrypted_message
-    
