f4aed253579e7a420b56f2a2c1caa2ff3f9b7211
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-22 22:59:03 +1000

    FIXED A LOTTA SHIT

--- app.py
+++ app.py
@@ -1,24 +1,50 @@
-from flask import Flask
+import argparse
+import importlib.util
+from flask import Flask, url_for
 from flask_session import Session
+from routes import index, user, chat
+from auth.middleware import log_request_info
 
+# app.py
+from create_app import create_app
 
-app = Flask(__name__)
-
-#configuring the session
-
-app.config['SESSION_PERMANENT']=False
-app.config['SESSION_TYPE']='filesystem'
-Session(app)
-
-#Import the routes
-
+app = create_app()
+app.register_blueprint(index.bp)
+app.register_blueprint(user.bp)
+app.register_blueprint(chat.bp)
+app.before_request(log_request_info)
 #error handling
-
 @app.errorhandler(500)
 def handle_internal_error(error):
     return 'Internal server error', 500
 
-from routes import index, chat, user
+@app.route('/routes')
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.parse.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    return "<br>".join(output)
+
+# Load routes/index.py using importlib
+spec = importlib.util.spec_from_file_location("index", "./routes/index.py")
+index = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(index)
+
+parser = argparse.ArgumentParser(description="Run the Flask app")
+parser.add_argument('--host', type=str, default='127.0.0.1',
+                    help='What host to listen on (default is 127.0.0.1)')
+parser.add_argument('--port', type=int, default=5000,
+                    help='What port to listen on (default is 5000)')
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, debug=False, use_reloader=False)
