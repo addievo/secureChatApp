3002f89b24d73c39796355385139211935df4ad0
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-22 20:46:43 +1000

    changed js display script.js

--- static/script.js
+++ static/script.js
@@ -1,18 +1,18 @@
 function fetchMessages() {
     fetch('/get_messages')
         .then(response => response.json())
-        .then(messages=> {
+        .then(decrypted_messages => {
             //clear old messages
             const messagesDiv = document.getElementById('messages');
             messagesDiv.innerHTML = '';
 
-            for (let message of messages){
+            for (let message of decrypted_messages){
                 const p = document.createElement('p');
-                p.textContent = message.content;
+                // Assuming the decrypted_messages array has the format (sender_username, receiver_username, decrypted_content)
+                p.textContent = `From ${message[0]}: ${message[2]}`; // Display the sender's name and the decrypted content
                 messagesDiv.appendChild(p);
             }
-        }
-    );
+        });
 }
 
 setInterval(fetchMessages, 1000);
