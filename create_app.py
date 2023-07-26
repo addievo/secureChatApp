35064ae66484da65822deb46232f92bd22e9ac78
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-26 14:12:18 +1000

    added CORS for check

--- create_app.py
+++ create_app.py
@@ -1,9 +1,10 @@
 # create_app.py
 from flask import Flask
 from flask_session import Session
-
+from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config['SESSION_PERMANENT'] = False
     app.config['SESSION_TYPE'] = 'filesystem'
     Session(app)
