826530d7a7e90069fe2748820e72b3faae77b27b
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-26 21:43:30 +1000

    changed fetch message delay to 0.5s4e

--- static/script.js
+++ static/script.js
@@ -218,5 +218,5 @@ function convertYouTubeURL(url) {
 
 
 setInterval(fetchConversations, 5000);
-setInterval(fetchMessages, 2500); // Fetch messages every second
+setInterval(fetchMessages, 500); // Fetch messages every second
 
