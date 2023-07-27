9980024e817a597e24cd83f4805294b8cb1a4151
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-27 20:02:55 +1000

    convo listted msessed up

--- static/script.js
+++ static/script.js
@@ -111,8 +111,7 @@ function fetchMessages(receiver_username) {
 document.getElementById('conversation-list').addEventListener('click', function(event) {
     // Get the username of the conversation that was clicked
     const username = event.target.textContent;
-
-    // Fetch messages for the new conversation
+    activeConversation = username;
 });
 
 
@@ -132,6 +131,24 @@ document.getElementById('start-conversation').addEventListener('click', function
 
     // Fetch messages for the new conversation
     fetchMessages(newConversationUsername);
+});
+
+
+socket.on('new_message', function(data) {
+    // Assuming `data` is an object with `sender_username` and `message` fields
+    if (data.sender_username === activeConversation) {
+        // If the new message is part of the active conversation, add it to the UI
+        addMessage(data);
+    }
+
+    // Add the new conversation to the conversation list if it's not already there
+    const conversationList = document.getElementById('conversation-list');
+    const existingConversations = Array.from(conversationList.getElementsByTagName('li')).map(li => li.textContent);
+    if (!existingConversations.includes(data.sender_username)) {
+        const listItem = document.createElement('li');
+        listItem.textContent = data.sender_username;
+        conversationList.appendChild(listItem);
+    }
 });
 
 
@@ -201,22 +218,7 @@ function addMessage(messageData) {
     // Append the new message to the chat
     messagesDiv.appendChild(messageElement);
 }
-socket.on('new_message', function(data) {
-    // Assuming `data` is an object with `sender_username` and `message` fields
-    if (data.sender_username === activeConversation) {
-        // If the new message is part of the active conversation, add it to the UI
-        addMessage(data);
 
-        // Add the new conversation to the conversation list if it's not already there
-        const conversationList = document.getElementById('conversation-list');
-        const existingConversations = Array.from(conversationList.getElementsByTagName('li')).map(li => li.textContent);
-        if (!existingConversations.includes(data.sender_username)) {
-            const listItem = document.createElement('li');
-            listItem.textContent = data.sender_username;
-            conversationList.appendChild(listItem);
-        }
-    }
-});
 let picker = document.querySelector('emoji-picker');
 let pickerEventAdded = false; // New variable to track whether the event has been added
 
