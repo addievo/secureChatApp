a05e5c544bda21a865bfd743f7e3c9a0d372a1e1
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-27 13:31:18 +1000

    firefox fix v1

--- static/script.js
+++ static/script.js
@@ -82,7 +82,7 @@ document.getElementById('send-message-form').addEventListener('submit', function
 document.getElementById('message').addEventListener('keydown', function(event) {
     if (event.keyCode === 13) { // keyCode for Enter key
         event.preventDefault(); // Prevent newline being added to textarea
-        document.getElementById('send-message-form').dispatchEvent(new Event('submit')); // Trigger form submission
+        document.getElementById('send-message-form').dispatchEvent(new Event('submit', { cancelable: true })); // Trigger form submission
     }
 });
 
