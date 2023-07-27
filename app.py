d2a8b650647c911b8317e857558eda97fc0dda59
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-07-28 00:50:58 +1000

    using waitress and signed certs for https

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, url_for
 from flask_session import Session
 from routes import index, user, chat
 from auth.middleware import log_request_info
+import ssl
+from waitress import serve
 
 # app.py
 from create_app import create_app, socketio
@@ -44,7 +46,17 @@ parser.add_argument('--host', type=str, default='127.0.0.1',
                     help='What host to listen on (default is 127.0.0.1)')
 parser.add_argument('--port', type=int, default=5000,
                     help='What port to listen on (default is 5000)')
+parser.add_argument('--cert', type=str, default='path_to_your_certificate.pem', help='Path to SSL certificate file')
+parser.add_argument('--key', type=str, default='path_to_your_privatekey.pem', help='Path to SSL private key file')
+
+
+def load_ssl_content(cert_path, key_path):
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
+    return ssl_context
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    socketio.run(app, host=args.host, port=args.port, debug=False, use_reloader=False)
+    ssl_context = load_ssl_content(args.cert, args.key)
+    serve(app, host=args.host, port=args.port, url_scheme='https', ssl_context=ssl_context)
