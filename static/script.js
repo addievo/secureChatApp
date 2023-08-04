ca719935c02e93f89ed46330442df2f4a4c9f65a
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-08-05 04:49:02 +1000

    changed websocket link frontend

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 let lastMessage = null;
 let activeConversation = null;
 let currentConversation = null;
-const socket = io.connect('https://chat.adityav.au', {
+const socket = io.connect('http://192.168.1.91:5201', {
     upgrade: false,
     transports: ['websocket']
 });
